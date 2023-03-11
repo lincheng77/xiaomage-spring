555abff889179cfc0c56251d062fb7c06ce0e963
lincheng77 <1498047434@qq.com>
2023-03-11 15:41:39 +0800

    依赖查找-延迟查找

--- thinking-in-spring/ioc-container-overview/src/main/java/edkso/cn/ioc/overview/dependency/DependencyLookupDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/edkso/cn/ioc/overview/dependency/DependencyLookupDemo.java
@@ -2,6 +2,7 @@ package edkso.cn.ioc.overview.dependency;
 
 import edkso.cn.ioc.overview.domain.User;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -14,8 +15,20 @@ public class DependencyLookupDemo {
         //启动 Spring 应用上下文
 
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-demo.xml");
-        User user = (User) beanFactory.getBean("user");
-
-        System.out.println(user);
+        lookInRealTime(beanFactory);
+        lookInLazy(beanFactory);
     }
+
+    private static void lookInLazy(BeanFactory beanFactory) {
+        ObjectFactory<User> objectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
+        User user = objectFactory.getObject();
+        System.out.println("延迟查找：" + user);
+    }
+
+    private static void lookInRealTime(BeanFactory beanFactory) {
+        User user = (User) beanFactory.getBean("user");
+        System.out.println("延迟查找：" + user);
+    }
+
+
 }
