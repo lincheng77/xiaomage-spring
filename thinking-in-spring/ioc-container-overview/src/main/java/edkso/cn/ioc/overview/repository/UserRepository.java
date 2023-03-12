f16e637ecae2204bbf49f236e6d613200718fabf
lincheng77 <1498047434@qq.com>
2023-03-12 16:49:08 +0800

    依赖注入 自动注入(根据类型 通过set方法注入)

--- thinking-in-spring/ioc-container-overview/src/main/java/edkso/cn/ioc/overview/repository/UserRepository.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/edkso/cn/ioc/overview/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package edkso.cn.ioc.overview.repository;
 
 import edkso.cn.ioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
 
 import java.util.Collection;
 
@@ -11,11 +12,23 @@ import java.util.Collection;
 public class UserRepository {
     private Collection<User> users;
 
+    private BeanFactory beanFactory;
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) {
+        System.out.println("正在注入--setBeanFactory");
+        this.beanFactory = beanFactory;
+    }
+
     public Collection<User> getUsers() {
         return users;
     }
 
     public void setUsers(Collection<User> users) {
+        System.out.println("正在注入--setUsers");
         this.users = users;
     }
 
